76d24d9260b29af377178a9c682d016e5ccfb5ed
Claire <claireserra37@gmail.com>
2017-04-25 12:24:11 +0200

    Sélection de plusieurs gamers OK

--- src/GameBundle/Controller/GameController.php
+++ src/GameBundle/Controller/GameController.php
@@ -11,15 +11,17 @@ class GameController extends Controller
 {
     public function playAction(Request $request)
     {
-        $id = $request->query->get('gamer');
+        $idArray = $request->query->get('gamer');
         $em = $this->getDoctrine()->getManager();
-        $gamer = $em->getRepository('UserBundle:Gamer')->find($id);
-
+        $gamers = [];
+        foreach ($idArray as $id) {
+            $gamers[] = $em->getRepository('UserBundle:Gamer')->find($id);
+        }
 
         return $this->render('GameBundle:Game:play.html.twig', array(
             'title'     => 'À vous de jouer !',
             'titleTab'  => 'Let\'s play !',
-            'gamer'      => $gamer
+            'gamers'      => $gamers
         ));
     }
 }
